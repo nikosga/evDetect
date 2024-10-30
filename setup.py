f0db7f05e0aa01b895cd94b765fc686982177a0e
Nikos Gavriil <nikosga123@gmail.com>
2024-10-30 23:04:39 +0400

    fix setup 2

--- setup.py
+++ setup.py
@@ -4,34 +4,6 @@ setup(
     name='evdetect',
     version='0.1.0',    
     description='Parametric event detection & inference library',
-    long_description='''
-    # evDetect
-    Parametric event detection & inference library
-
-    ## Install
-
-    ```
-    pip install evdetect
-    ```
-
-    ## How to use
-
-    **Example**
-
-    ```python
-    from evdetect.evdetector import Detector
-    from evdetect.gen_data import Scenario
-
-    s = Scenario()
-    d=Detector()
-    d.fit(s.data)
-    print(d.summary())
-    d.predict()
-    d.plot()
-    ```
-
-    For more examples see the tutorial in the notebooks folder.
-    ''',
     url='https://github.com/nikosga/evDetect/tree/main',
     author='Nick Gavriil',
     license='Apache-2.0',
