16a7f17bb030c80ed955c359f013e95430e3f8fc
Nikos Gavriil <nikosga123@gmail.com>
2024-10-30 22:59:46 +0400

    updating setup

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(
     name='evdetect',
     version='0.1.0',    
     description='Parametric event detection & inference library',
+    long_description=long_description,
     url='https://github.com/nikosga/evDetect/tree/main',
     author='Nick Gavriil',
     license='Apache-2.0',
